40ab8e7ca856c8a29204f3a151660ef3a3e0aeb6
Chad Hurd <gibble@gmail.com>
2026-01-13 17:49:57 -0600

    fixed?

---
+++ api/src/functions/index.ts
@@ -0,0 +1,12 @@
+// Entry point to ensure all functions are registered with the Azure Functions runtime.
+import './GetAnnouncements';
+import './GetBowlerResults';
+import './GetBowlers';
+import './GetContentBlocks';
+import './GetSchedule';
+import './GetSeasons';
+import './GetTournamentResults';
+import './GetTournaments';
+import './PutTournamentResults';
+import './SaveContentBlock';
+import './WhoAmI';
