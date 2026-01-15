5cfd2292a45ebc62686026b9a2461b543742d2be
Chad Hurd <gibble@gmail.com>
2026-01-15 11:09:45 -0600

    fix auth?

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,9 +1,11 @@
 import config from '../../auth_config.prod.json';
 
-const { domain, clientId, audience, apiUri, errorPath } = config as {
+const { domain, clientId, authorizationParams: { audience }, apiUri, errorPath } = config as {
   domain: string;
   clientId: string;
-  audience?: string;
+  authorizationParams: {
+    audience?: string;
+  },
   apiUri: string;
   errorPath: string;
 };
@@ -26,7 +28,7 @@ export const environment = {
     clientId,
     authorizationParams: {
       ...(audience && audience !== "YOUR_API_IDENTIFIER" ? { audience } : null),
-      redirect_uri: `${window.location.origin}/ManitobaMasterBowlers`,
+      redirect_uri: window.location.origin,
     },
     errorPath,
   },
